b86e6b6546c9b20d75ed97272d34e16f40332c81
radchaponsr-debug <radchapon.sr@tdshop.io>
2026-07-10 14:49:40 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-JavaScript (ดู guide)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
